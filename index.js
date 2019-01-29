66f6f8a16959fc9d03670d31a91a62d57da3ce52
Joe <joe45moses@gmail.com>
2019-01-29 15:27:55 +0000

    DB removal

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // mysql
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'message-app'
-});
-connection.connect();
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'message-app'
+// });
+// connection.connect();
 const express = require('express');
 const socket = require('socket.io');
 //app setup
@@ -20,19 +20,19 @@ var io = socket(server);
 io.on('connect', function(socket) {
     var seshid = socket.id;
     connection.query('SELECT * from messages ORDER BY id DESC LIMIT 30', function(err, rows, fields) {
-        if (!err) {
-            io.emit('reply', rows)
-            console.log(rows[0].msg_from + '- result');
-        } else console.log('Error while performing Query.');
+        // if (!err) {
+        //     io.emit('reply', rows)
+        //     console.log(rows[0].msg_from + '- result');
+        // } else console.log('Error while performing Query.');
     });
     socket.on('message', function(data) {
         io.emit('message', data)
         // send to db
-        var sql = `INSERT INTO messages (msg_from, msg_content) VALUES ('${data.user}', '${data.content}')`;
-        connection.query(sql, function(err, result) {
-            if (err) console.log('error');
-            console.log("1 record inserted");
-        });
+        // var sql = `INSERT INTO messages (msg_from, msg_content) VALUES ('${data.user}', '${data.content}')`;
+        // connection.query(sql, function(err, result) {
+        //     if (err) console.log('error');
+        //     console.log("1 record inserted");
+        // });
     });
     // istyping
     socket.on('istyping', () => {
